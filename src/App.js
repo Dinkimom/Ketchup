7a37a3ff4f0e3ad366a421f1312a06f494631bc1
Nikita Dmitriev <nikita.dmitriev@corp.mail.ru>
2023-09-17 13:59:48 +0300

    feat: add init skeleton

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
 /*global chrome*/
 
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          {this.props.isExt ? 
-            <img src={chrome.runtime.getURL("static/media/logo.svg")} className="App-logo" alt="logo" />
-          :
-            <img src={logo} className="App-logo" alt="logo" />
-          }
+const App = ({ isExt }) => {
+  const [on, setOn] = useState(false);
 
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+  const handleToggleOn = () => {
+    setOn(!on);
+  };
+
+  return (
+    <div className="App">
+      <button onClick={handleToggleOn}>{on ? "Off" : "On"}</button>
+    </div>
+  );
+};
 
 export default App;
